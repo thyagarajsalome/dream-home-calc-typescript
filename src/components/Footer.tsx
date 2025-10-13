bc787ba86b3ffcaf4ba01defa71b7fcb62e06456
https://github.com/thyagarajsalome/KQ-V1.0.0.git <thyagarajsalome@gmail.com>
2025-10-13 13:55:44 +0530

    updated-header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,24 +7,7 @@ export default function Footer() {
         <a href="#home" className="logo" style={{ color: "#fff" }}>
           <i className="fas fa-home"></i> DreamHomeCalc
         </a>
-        <ul className="social-links">
-          <li>
-            <a href="#" aria-label="Facebook">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#" aria-label="Twitter">
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#" aria-label="Instagram">
-              <i className="fab fa-instagram"></i>
-            </a>
-          </li>
-        </ul>
-        <p style={{ marginTop: "1rem" }}>
+        <p style={{ marginTop: "0.5rem" }}>
           &copy; 2025 DreamHomeCalc. All rights reserved.
         </p>
       </div>
