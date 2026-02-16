3b5993114f4e755de757910862200e006231f436
Thyagaraja <your-github-email@example.com>
2026-02-16 16:44:02 +0530

    Setup GitHub Pages with custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dream-home-calc-typescript/", // REPLACE THIS with your actual repo name if different
+  // For custom domains (homedesignenglish.com), this must be '/'
+  base: "/", 
 })
