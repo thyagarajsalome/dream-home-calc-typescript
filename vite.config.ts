ab058e478924da890125be3df44bb288f707dc34
Thyagaraja <your-github-email@example.com>
2026-02-17 08:16:42 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // For custom domains (homedesignenglish.com), this must be '/'
-  base: "/", 
+  base: "/",
+  build: {
+    chunkSizeWarningLimit: 1000, // Increases limit to 1000 kB to hide the warning
+  }
 })
