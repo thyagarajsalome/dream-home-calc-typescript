ba13eacd1121dffb0524e32c6b3e216f8722ee85
Thyagaraja <your-github-email@example.com>
2026-04-23 13:57:48 +0530

    firebase-shift

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // CHANGED: Set base to your repository name for GitHub Pages default URL
-  base: "/", 
+  // Removed 'base: "/"' as it is the default for root domain hosting on Firebase
   server: {
     port: 3000, 
     strictPort: true, 
