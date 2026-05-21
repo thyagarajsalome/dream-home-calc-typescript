fbf10d0c66bc790fc6a2cc3a35bf3de29f1dcfa8
Thyagaraja <your-github-email@example.com>
2026-05-21 07:04:24 +0530

    migrated-atro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    react(),
-    tailwind({
-      configFile: './tailwind.config.js',
-    })
+    react()
   ],
   vite: {
     envPrefix: 'VITE_',
